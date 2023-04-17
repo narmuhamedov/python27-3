4edf92d8e840d40abecda414597d6b9855968334
rz <kingradomir@gmail.com>
2023-04-17 17:32:46 +0600

    html_css

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     image = models.ImageField(upload_to='')
     description = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.title
